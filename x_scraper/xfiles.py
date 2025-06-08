78424f7a729650218bb9bbc389e16709eafd1d5d
Minh Nguyen <39523821+smackfunyc@users.noreply.github.com>
2025-06-07 23:28:46 -0400

    Create xfiles.py

---
+++ x_scraper/xfiles.py
@@ -0,0 +1,10 @@
+# add in your X api credentials here
+
+
+twitter_acc ='Maximus_Cubicus'
+api_key='123456789'
+api_secret='123456789'
+access_token='123456789'
+access_token_secret='123456789'
+bearer_token='123456789'
+
